d97078127aec2e81c2264d33fadfedf233a21213
unknown <freshstarter18@gmail.com>
2019-05-25 14:07:53 +0800

    updated spending cart

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -13,22 +13,27 @@ export default class HomeScreen extends React.Component {
   };
   
   render() {
+    const date = new Date()
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <Text style={{ fontSize: 30, color: "white", fontWeight: "bold"}}>
-            Hello World 
-            {"\n"}
-            {"\n"}
-            Powered by React Native
-            {"\n"}
-            {"\n"}
-            Magic 25.05.19 
-            </Text>
+        <View style={styles.card}>
+            <View style={styles.cardDate}>
+              <Text>{date.toLocaleDateString()}</Text>
+            </View>
+            <View style={styles.cardRow}>
+              <Text>Food</Text>
+              <Text>USD 8</Text>
+            </View>
+            <View style={styles.cardRow}>
+              <Text>Grocery</Text>
+              <Text>USD 2</Text>
+            </View>
+          </View>
         </ScrollView>
         <View style={styles.tabBarInfoContainer}>
           <Text style={{fontWeight: 'bold'}}>Total</Text>
-          <Text style={{fontWeight: 'bold'}}>RM 10</Text>
+          <Text style={{fontWeight: 'bold'}}>USD 10</Text>
         </View>
       </View>
     );
@@ -41,10 +46,26 @@ const styles = StyleSheet.create({
     backgroundColor: 'grey',
   },
   contentContainer: {
-    paddingTop: 30,
-    alignItems: 'center'
+  //  alignItems: 'center'
   },
-
+  card: {
+    borderWidth: 0.5,
+    borderColor: '#d6d7da'
+  },
+  cardDate: {
+    padding: 10,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+    backgroundColor: '#f5f5f5'
+  },
+  cardRow: {
+    padding: 10,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+  },		
   tabBarInfoContainer: {
     position: 'absolute',
     bottom: 0,
